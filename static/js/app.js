793426d2d66249f116bdb911b9a3d040fec1b870
Margaret Sy <margaretsy1016@gmail.com>
2015-07-07 22:09:06 -0700

    circle-btn-play yolo

--- static/js/app.js
+++ static/js/app.js
@@ -19,6 +19,24 @@ var main = function() {
     displayContent.siblings().removeClass('active');
   });
 
+  $('.circle-btn-play').click(function() {
+    $(this).removeClass('active-circle');
+    var nextActiveIndex = getRandomInt(0, $(this).siblings().length);
+    var nextActive = $(this).siblings().eq(nextActiveIndex);
+    nextActive.addClass('active');
+  });
+
+  $('.circle-btn-play').hover(function() {
+    $(this).removeClass('active-circle');
+    var nextActiveIndex = getRandomInt(0, $(this).siblings().length);
+    var nextActive = $(this).siblings().eq(nextActiveIndex);
+    nextActive.addClass('active');
+  });
+
 };
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 $(document).ready(main);
