37ef1c0b51ee40afc3c65553eca18153e88dece5
Margaret Sy <margaretsy1016@gmail.com>
2015-07-07 20:07:40 -0700

    change misc.html to berkeley.html

--- site.py
+++ site.py
@@ -17,13 +17,11 @@ freezer = Freezer(app)
 app.config.from_object(__name__)
 
 
-blog_home = '/posts'
-
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/posts/")
+@app.route("/posts.html")
 def posts():
     posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
     posts.sort(key=lambda item:item['date'], reverse=True)
@@ -39,9 +37,9 @@ def post(name):
 def projects():
     return render_template('projects.html')
 
-@app.route("/misc.html")
-def misc():
-    return render_template('misc.html')
+@app.route("/berkeley.html")
+def berkeley():
+    return render_template('berkeley.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
