2526ad597a0f9bdec98d9e3708d535c34849dc15
Margaret Sy <margaretsy1016@gmail.com>
2015-09-09 22:06:46 -0700

    add hard-coded 404 page

--- site.py
+++ site.py
@@ -78,6 +78,12 @@ def berkeley():
 def page_not_found(e):
     return render_template('404.html'), 404
 
+# for static site
+@freezer.register_generator
+def error_handlers():
+    yield "/404"
+
+# only used with live site
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
